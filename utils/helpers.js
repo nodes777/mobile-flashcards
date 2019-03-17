73591a881a02f9c9022c78c0967bbec99d36a287
Taylor <taylor.nodell@gmail.com>
2019-03-17 12:48:25 -0400

    inital commit

---
+++ utils/helpers.js
@@ -0,0 +1,12 @@
+export const getDecks = () => {
+	//return all of the decks along with their titles, questions, and answers.
+};
+export const getDeck = id => {
+	//take in a single id argument and return the deck associated with that id.
+};
+export const saveDeckTitle = title => {
+	//take in a single title argument and add it to the decks.
+};
+export const addCardToDeck = (title, card) => {
+	//will add the card to the list of questions for the deck with the associated title.
+};
